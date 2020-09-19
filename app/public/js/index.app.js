46e4d4614400991f23ad7ea42fb947005e470dc3
Kolton <koltontebbey@gmail.com>
2020-09-18 20:29:06 -0400

    Actually uploading the files
    lol big brain commit message

---
+++ app/public/js/index.app.js
@@ -0,0 +1,42 @@
+var app = new Vue({
+    el: '#notSoFunky', //Double check element
+  data: {
+    name: '',
+    lastName: '',
+    email: '',
+    country: '',
+    birthday: '',
+    age: '',
+    userImgLg: '',
+    userImgTmb: ''
+  },
+  created() {
+    this.PullData();
+  },
+  methods: {
+    PullData: function() {
+        fetch("https://randomuser.me/api")
+        .then( response => response.json() )
+        .then( json => {
+          //this.ptList = json;
+          let data = json.results[0];
+        
+          console.log(data);
+
+          this.name = data.name.first + " " + data.name.last;
+          this.country = data.location.country;
+          this.email = data.email;
+          this.birthday = new Date(data.dob.date);
+          this.age = data.dob.age;
+          this.userImgLg = data.picture.large;
+          this.userImgTmb = data.picture.thumbnail;
+          
+          console.log(this.userImgTmb);
+        }
+    
+        );
+      }
+    }
+  
+  
+})
